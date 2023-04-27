be29366307d2512c23fd52a115b86b557109caf4
Gianluca Natali <8244206+gianlucanatali@users.noreply.github.com>
2023-04-28 00:57:05 +0200

    Update aws_main.tf

--- streaming-pacman/terraform/aws/aws_main.tf
+++ streaming-pacman/terraform/aws/aws_main.tf
@@ -39,6 +39,15 @@ resource "aws_s3_bucket" "pacman" {
   
 }
 
+resource "aws_s3_bucket_public_access_block" "pacman" {
+  bucket = aws_s3_bucket.pacman.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_ownership_controls" "pacman" {
   bucket = aws_s3_bucket.pacman.id
   rule {
